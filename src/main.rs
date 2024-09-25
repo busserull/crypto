f08ec0ebd7304548f13c3268af908a0aa3290b8a
Kolbjørn Austreng <kolbjorn.austreng@radionor.no>
2024-09-25 13:08:59 +0200

    wip
    Signed-off-by: Kolbjørn Austreng <kolbjorn.austreng@radionor.no>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 mod aes;
 mod base64;
 mod chunk_pair_iter;
@@ -5,14 +7,13 @@ mod key_value;
 mod pkcs7;
 mod urandom;
 
-use aes::{aes_ecb_decrypt, aes_ecb_encrypt, AesKey};
+use aes::AesKey;
 use chunk_pair_iter::ChunkPairIter;
-use key_value::KeyValue;
 
 use std::collections::HashMap;
 use std::fmt;
 use std::fs;
-use std::hint::black_box;
+use std::io::BufRead;
 use std::path::Path;
 
 struct Buffer(Vec<u8>);
@@ -233,270 +234,14 @@ fn single_xor_key_decipher(buffer: Buffer) -> (f64, u8, Buffer) {
     (lowest_penalty, best_key, deciphered)
 }
 
-fn encryption_oracle<H, I>(head: H, cleartext: I, key: &AesKey) -> Vec<u8>
-where
-    H: AsRef<[u8]>,
-    I: AsRef<[u8]>,
-{
-    let random_byte_count = urandom::range(5, 19);
-    let random_bytes = urandom::bytes(random_byte_count as usize);
-
-    let to_encrypt: Vec<u8> = random_bytes
-        .into_iter()
-        .chain(head.as_ref().into_iter().copied())
-        .chain(cleartext.as_ref().into_iter().copied())
-        .collect();
-
-    aes::aes_ecb_encrypt(&to_encrypt, key)
-}
-
-fn find_last_repeated_block(input: &[u8], block_size: usize) -> Option<usize> {
-    if 2 * block_size > input.len() {
-        return None;
-    }
-
-    let mut found: Option<usize> = None;
-
-    for (i, head) in input.chunks_exact(block_size).enumerate() {
-        for (j, next) in input.chunks_exact(block_size).enumerate().skip(i + 1) {
-            match (found.is_some(), head == next) {
-                (_, true) => found = Some(j * block_size),
-                (true, false) => return found,
-                _ => (),
-            }
-        }
-    }
-
-    found
-}
-
-fn snoop_block_count(text: &[u8], key: &AesKey, block_size: usize) -> usize {
-    let mut length_test_cipher = vec![0];
-    let mut last_zero_block_start: Option<usize> = None;
-
-    while last_zero_block_start.is_none() {
-        length_test_cipher = encryption_oracle(&vec![0; 2 * block_size], &text, &key);
-        last_zero_block_start = find_last_repeated_block(&length_test_cipher, block_size);
-    }
-
-    (length_test_cipher.len() - last_zero_block_start.unwrap() - block_size) / block_size
-}
-
-fn create_oracle_marker(text: &[u8], key: &AesKey, plaintext_marker: &[u8]) -> Vec<u8> {
-    let block_size = plaintext_marker.len();
-
-    let test: Vec<u8> = [0]
-        .repeat(2 * block_size)
-        .into_iter()
-        .chain(plaintext_marker.iter().copied())
-        .collect();
-
-    let mut test_cipher = vec![0];
-    let mut last_zero_block_start: Option<usize> = None;
-
-    while last_zero_block_start.is_none() {
-        test_cipher = encryption_oracle(&test, &text, &key);
-        last_zero_block_start = find_last_repeated_block(&test_cipher, block_size);
-    }
-
-    let i = last_zero_block_start.unwrap();
-
-    Vec::from(&test_cipher[i + block_size..i + 2 * block_size])
-}
-
-fn find_marker(input: &[u8], marker: &[u8]) -> Option<usize> {
-    for (i, next) in input.chunks_exact(marker.len()).enumerate() {
-        if next == marker {
-            return Some(i * marker.len());
-        }
-    }
-
-    None
-}
-
-struct QuoteOutIter<'a> {
-    input: std::slice::Iter<'a, u8>,
-    escaped: Option<u8>,
-}
-
-impl<'a> QuoteOutIter<'a> {
-    fn new(input: &'a [u8]) -> Self {
-        Self {
-            input: input.iter(),
-            escaped: None,
-        }
-    }
-}
-
-impl<'a> Iterator for QuoteOutIter<'a> {
-    type Item = u8;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if let Some(byte) = self.escaped {
-            self.escaped = None;
-            return Some(byte);
-        }
-
-        let next = self.input.next();
-
-        match next {
-            Some(b';') | Some(b'=') => {
-                self.escaped = next.copied();
-                Some(b'\\')
-            }
-
-            Some(&byte) => Some(byte),
-
-            None => None,
-        }
-    }
-}
-
-fn submit_userdata(input: &[u8], key: &AesKey, iv: &[u8]) -> Vec<u8> {
-    let cleartext: Vec<u8> = b"comment1=cooking%20MCs;userdata="
-        .into_iter()
-        .copied()
-        .chain(QuoteOutIter::new(input).into_iter())
-        .chain(
-            b";comment2=%20like%20a%20pound%20of%20bacon"
-                .into_iter()
-                .copied(),
-        )
-        .collect();
-
-    aes::aes_cbc_encrypt(&cleartext, key, iv).unwrap()
-}
-
-fn is_admin(input: &[u8], key: &AesKey, iv: &[u8]) -> bool {
-    let decrypted = aes::aes_cbc_decrypt(input, key, iv).unwrap();
-
-    decrypted
-        .windows(12)
-        .filter_map(|w| (w == b";admin=true;").then_some(true))
-        .next()
-        .is_some()
-}
-
-struct CbcPaddingOracle {
-    key: AesKey,
-    iv: Vec<u8>,
-    cleartext: Buffer,
-}
-
-impl CbcPaddingOracle {
-    fn new(string: usize) -> Self {
-        let strings = [
-            "MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc=",
-            "MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic=",
-            "MDAwMDAyUXVpY2sgdG8gdGhlIHBvaW50LCB0byB0aGUgcG9pbnQsIG5vIGZha2luZw==",
-            "MDAwMDAzQ29va2luZyBNQydzIGxpa2UgYSBwb3VuZCBvZiBiYWNvbg==",
-            "MDAwMDA0QnVybmluZyAnZW0sIGlmIHlvdSBhaW4ndCBxdWljayBhbmQgbmltYmxl",
-            "MDAwMDA1SSBnbyBjcmF6eSB3aGVuIEkgaGVhciBhIGN5bWJhbA==",
-            "MDAwMDA2QW5kIGEgaGlnaCBoYXQgd2l0aCBhIHNvdXBlZCB1cCB0ZW1wbw==",
-            "MDAwMDA3SSdtIG9uIGEgcm9sbCwgaXQncyB0aW1lIHRvIGdvIHNvbG8=",
-            "MDAwMDA4b2xsaW4nIGluIG15IGZpdmUgcG9pbnQgb2g=",
-            "MDAwMDA5aXRoIG15IHJhZy10b3AgZG93biBzbyBteSBoYWlyIGNhbiBibG93",
-        ];
-
-        let cleartext = Buffer::from_base64(strings[string]);
-
-        let key = AesKey::from(&urandom::bytes(16)).unwrap();
-
-        let iv = urandom::bytes(16);
-
-        Self { key, iv, cleartext }
-    }
-
-    fn iv(&self) -> Vec<u8> {
-        self.iv.clone()
-    }
-
-    fn ciphertext(&self) -> Vec<u8> {
-        aes::aes_cbc_encrypt(self.cleartext.as_ref(), &self.key, &self.iv).unwrap()
-    }
-
-    fn correctly_padded(&self, ciphertext: &[u8]) -> bool {
-        aes::aes_cbc_decrypt(&ciphertext, &self.key, &self.iv).is_ok()
-    }
-}
-
-fn shape_tail(cipher_block: &mut [u8], zero_iv: &[u8], tail: u8) {
-    for (cipher_byte, known) in cipher_block.iter_mut().zip(zero_iv.iter()) {
-        *cipher_byte ^= known;
-    }
-
-    for cipher_byte in cipher_block.iter_mut().rev().take(tail as usize - 1) {
-        *cipher_byte ^= tail;
-    }
-}
-
-fn last_cleartext_byte(oracle: &CbcPaddingOracle, two_blocks: &[u8; 32]) -> u8 {
-    let mut ws: [u8; 32] = [0; 32];
-    ws.copy_from_slice(two_blocks);
-
-    for byte in 1..u8::MAX {
-        ws[15] ^= byte;
-
-        if oracle.correctly_padded(&ws) {
-            ws[14] ^= 0x01;
-
-            if oracle.correctly_padded(&ws) {
-                return byte;
-            }
-
-            ws[14] ^= 0x01;
-        }
-
-        ws[15] ^= byte;
-    }
-
-    unreachable!();
-}
-
-fn decrypt_last_block(oracle: &CbcPaddingOracle, two_blocks: &[u8]) -> Vec<u8> {
-    let mut ws: [u8; 32] = [0; 32];
-    ws.copy_from_slice(two_blocks.try_into().unwrap());
-
-    let mut zero_iv = vec![0; 16];
-    zero_iv[15] = last_cleartext_byte(oracle, &ws) ^ 0x01;
-
-    for i in (0..15).rev() {
-        let target_byte = 16 - i as u8;
-
-        shape_tail(&mut ws[0..16], &zero_iv, target_byte);
-
-        let mut byte = 0;
-
-        loop {
-            ws[i] ^= byte;
-
-            let target_reached = oracle.correctly_padded(&ws);
-
-            ws[i] ^= byte;
-
-            if target_reached {
-                break;
-            }
-
-            byte += 1;
-        }
-
-        shape_tail(&mut ws[0..16], &zero_iv, target_byte);
-
-        zero_iv[i] = byte ^ target_byte;
-    }
-
-    Vec::from(zero_iv)
+fn random_aes_128_key() -> AesKey {
+    AesKey::from(&urandom::bytes(16)).unwrap()
 }
 
 fn main() {
-    let ciphertext = Buffer::from_base64(
-        "L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ==",
-    );
+    let cleartexts = fs::read("19.txt").unwrap();
 
-    let key = AesKey::from(b"YELLOW SUBMARINE").unwrap();
-
-    let cleartext = aes::aes_ctr(&ciphertext, &key, 0);
-
-    println!("{}", String::from_utf8_lossy(&cleartext));
+    for (i, line) in cleartexts.lines().enumerate() {
+        println!("{:>2} {}", i, line.unwrap());
+    }
 }
